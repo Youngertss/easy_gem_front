aa205dd00931e4f111328460e52a454015a0523a
Youngertss <bambukov17@gmail.com>
2025-08-24 18:15:49 +0200

    succesful connection to ws

--- src/Components/Chat/Chat.tsx
+++ src/Components/Chat/Chat.tsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import s from "./Chat.module.scss"
 
+import { useUserStore } from '../../store/userStore';
+
 export const Chat = () => {
+    const messageInput = useRef(null)
+    const user = useUserStore((state) => state.user)
+
+    const ws = new WebSocket(`ws://127.0.0.1:8000/ws/chat/`);
+    ws.onopen = () => {
+        console.log("WebSocket connection established");
+    };
+
+    ws.onmessage = (e) => {
+        console.log("Message from server ", e.data);
+        const messageElement = document.createElement("div");
+        messageElement.className = s.message;
+        
+    };
+
+    const sendMessageHandler = () => {
+        if (!user) {
+            alert("First sign in to use chat");
+            return;
+        }
+        if (messageInput.current) {
+            const input = messageInput.current as HTMLInputElement;
+            console.log("Message to send:", input.value);
+            const data = { "message": input.value, "author": user.username };
+            ws.send(JSON.stringify(data));
+            input.value = "";
+        }
+    };
     return (
-        <div className={s.chatBlock}>
+        <div id="chatBlock" className={s.chatBlock}>
             <div className={s.sendMessageBlock}>
-                <input type="text" />
+                <input type="text" ref={messageInput} onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        sendMessageHandler();
+                    }
+                }} />
+                <img src={`/imgs/icons/sendMessageIcon.png`} alt="send" onClick={sendMessageHandler} />
             </div>
+
             <div className={s.message}>
-                <b><a className={s.messageAuthor}>youngerts:</a></b><span>Hi ewr1</span>
-            </div>
-            <div className={s.message}>
-                <b><a className={s.messageAuthor}>lololoshka:</a></b><span>Gl gg good one</span>
-            </div>
-            <div className={s.message}>
-                <b><a className={s.messageAuthor}>youngerts:</a></b><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi dolorem blanditiis voluptate quasi tempore minima! Ducimus, eos. Dolorum, ipsum, nisi velit unde ad et beatae ut tempora quam officiis quos?</span>
+                <u><b><a className={s.messageAuthor}>Server:</a></b></u><span>Don't forget to be polite. Have a fun!</span>
             </div>
         </div>
     );
