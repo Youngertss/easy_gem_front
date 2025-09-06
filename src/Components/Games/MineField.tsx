81af031f2bef39482bfe91229b788ce57bf64ea2
Youngertss <bambukov17@gmail.com>
2025-09-06 17:13:20 +0200

    MineField !static! is done

--- src/Components/Games/MineField.tsx
+++ src/Components/Games/MineField.tsx
@@ -9,10 +9,24 @@ interface MineFieldProps{
 
 export const MineField: React.FC<MineFieldProps> = ({gameStarted}) =>{
     return(
-        <div className={s.mineField}>
-            {Array.from({ length: 25 }).map((_, idx) => (
-                <div key={idx} className={s.mineCell}></div>
-            ))}
+        <div className={s.mineFieldLayer}>
+            <div className={s.mineField}>
+                {Array.from({ length: 25 }).map((_, idx) => {
+                    let rotation = 0;
+                    if (gameStarted){
+                        rotation = Math.floor(Math.random() * 360);
+                    };
+                    return (
+                        <div key={idx} className={s.mineCell}>
+                        <img
+                            src={"/imgs/icons/rocks.svg"}
+                            style={{ transform: `rotate(${rotation}deg)` }}
+                            alt=""
+                        />
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
